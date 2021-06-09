310e9561e3e3c96e1721a3c6fef0b0f52c10b629
arbimauday <arbimauday@gmail.com>
2021-06-09 09:29:47 +0700

    push new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React, {Component} from 'react'
 import './App.css';
+import { Switch, BrowserRouter as Router } from 'react-router-dom';
+import Layout from './layouts/index'
+import {publicRoutes, authRoutes, blankRoutes} from './routes/'
+import AppRoute from './routes/route'
+import "./assets/scss/theme.scss"
+import './thirdParty/firebase/firebase-messaging-sw'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const loopingRoute = (dataRoutes = [], customKey = 'route', isAuthProtected = false) => {
+  return dataRoutes.map((route, idx) => (
+      <AppRoute path={route.path}
+                layout={Layout}
+                component={route.component}
+                key={customKey + idx}
+                isAuthProtected={isAuthProtected}/>
+  ))
+}
+
+class App extends Component {
+
+  render() {
+
+    return (
+        <React.Fragment>
+            <Router>
+                <Switch>
+                    {loopingRoute(publicRoutes, 'route-public')}
+                    {loopingRoute(authRoutes, 'route-auth', true)}
+                    {loopingRoute(blankRoutes, 'route-blank')}
+                </Switch>
+            </Router>
+        </React.Fragment>
+    )
+  }
 }
 
 export default App;
